cb6dba486b8433dbbe86b5cc8b41641dd40bef69
Md. Habibur Rahman Shohel <md.hrshohel@gmail.com>
2023-05-14 01:30:35 +0600

    home jsx added

---
+++ src/Pages/Home/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Banner from '../Banner/Banner';
+import Menu from '../Popular Menu/PopularMenu';
+
+const Home = () => {
+  return (
+    <div>
+      <Banner></Banner>
+      <Menu></Menu>
+    </div>
+  );
+};
+
+export default Home;
